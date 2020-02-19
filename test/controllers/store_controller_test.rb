f8134438106edbc60b997e9650c1949b1c9c896c
Mia Sinek <mia@miasinek.com>
2020-02-19 17:45:00 +0100

    Functional testing of store controller

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.menu a', minimum: 4
+    assert_select 'div.catalog div.product-wrapper', 3
+    assert_select 'h2', 'To Kill a Mockingbird'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
-
 end
